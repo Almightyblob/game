4f9c5897beb8e7e9fee1a3e576191b66bfae1bbd
almightyblob <dominik.antunovic@gmail.com>
2019-12-01 15:47:51 +0100

    added ability to pick up blimsy

--- javascripts/intro.js
+++ javascripts/intro.js
@@ -10,11 +10,16 @@ class Box {
         this.x = x;
         this.y = y;
         this.color = color;
+        this.exists = true;
     }
     draw() {
+        if (this.exists === true){
         context.fillStyle = this.color;
         context.beginPath();
-        context.fillRect(this.x, this.y, this.width, this.height);       
+        context.fillRect(this.x, this.y, this.width, this.height);
+    } else {
+        return;
+    }       
     }
     get bottom(){
         return this.y + this.height;
@@ -51,30 +56,30 @@ class ColBox extends Box {
     }
 
     collisionRessolve(obj) {
-        var vector_x, vector_y;
+        var difference_x, difference_y;
         // get the distance between center points
-        vector_x = this.x_center - obj.x_center;
-        vector_y = this.y_center - obj.y_center;
-        console.log(vector_x, vector_y);
+        difference_x = this.x_center - obj.x_center;
+        difference_y = this.y_center - obj.y_center;
+        console.log(difference_x, difference_y);
         // is the y vector longer than the x vector?
-        if (vector_y * vector_y > vector_x * vector_x) {// square to remove negatives
+        if (Math.abs(difference_y / this.height) > Math.abs(difference_x / this.width)) {// square to remove negatives
     
           // is the y vector pointing down?
-          if (vector_y > 0) {
+          if (difference_y > 0) {
     
-            obj.y = this.y - this.height;
+            obj.y = this.y - obj.height;
             obj.y_velocity = 0;
             obj.jumping = false;
           } else { // the y vector is pointing up
     
             obj.y = this.y + this.height;
-    
+            obj.y_velocity = 0;
           }
     
         } else { // the x vector is longer than the y vector
     
           // is the x vector pointing right?
-          if (vector_x > 0) {
+          if (difference_x > 0) {
             obj.x = this.x - obj.width;
     
           } else { // the x vector is pointing left
@@ -106,11 +111,6 @@ class Character extends Box {
     }
 }
 
-var vadid = new Character (36, 36, 144, 0, "#ff0000", true, 0, 0, 0.5, 0.8, 20);
-var blimsy = new Character (20, 20, 30, 0, "#00ff00", true, 0, 0, 0.3, 0.5, 20);
-var block = new ColBox(36, 36, 200, 180 - 36, "#606060");
-
-
 var worldFunctions = {
     moveBlimsy(obj, player){
 
@@ -129,11 +129,6 @@ var worldFunctions = {
         obj.x_velocity *= 0.9;
         obj.y_velocity *= 0.9;
     
-        if (obj.y > 180 - obj.height){
-            obj.jumping = false;
-            obj.y = 180 - obj.height;
-            obj.y_velocity = 0;
-        }
     },
     checkBounds(obj) {
         if (obj.x > 320){
@@ -151,21 +146,29 @@ var worldFunctions = {
 var controller = {
     left: false,
     right: false,
-    up: false,
+    jump: false,
     shift: false,
+    up: false,
+    down: false,
     keyListener:function(event) {
         
         var keyState = (event.type == "keydown")? true:false;
 
         switch(event.code) {
+            case "KeyW":
+                controller.up = keyState;
+            break;
             case "KeyA":
                 controller.left = keyState;
             break;
-            case "Space":
-                controller.up = keyState;
+            case "KeyS":
+                controller.down = keyState;
             break;
             case "KeyD":
                 controller.right = keyState;
+            break;
+            case "Space":
+                controller.jump = keyState;
             break;
             case "ShiftLeft":
                 controller.shift = keyState;
@@ -173,7 +176,7 @@ var controller = {
         }
     },
     controls(obj){
-        if (controller.up && obj.jumping == false) {
+        if (controller.jump && obj.jumping == false) {
             obj.jumping = true;
             obj.y_velocity -= obj.jump;
         }
@@ -191,6 +194,15 @@ var controller = {
     
         if (controller.right && controller.shift){
             obj.x_velocity += obj.run;
+        }
+        if (vadid.collisionTest(blimsy) && controller.up){
+            blimsy.exists = false;            
+        } 
+        if (blimsy.exists === false && controller.down){
+            blimsy.x = obj.x;
+            blimsy.y = obj.y;
+            blimsy.exists = true;
+            
         }
     
         obj.y_velocity += 1.5;
@@ -210,30 +222,59 @@ var controller = {
     }
 };
 
-
+var vadid = new Character (36, 36, 144, 0, "#ff0000", true, 0, 0, 0.5, 0.8, 20);
+var blimsy = new Character (20, 20, vadid.x, vadid.y, "#00ff00", true, 0, 0, 0.3, 0.5, 20);
+var block = new ColBox(36, 36, 200, 180 - 36, "#606060");
+var rectangle = new ColBox(36, 70, 240, 120, "#606060");
+var block2 = new ColBox(36, 36, 200, 80 - 36, "#606060");
+var ground = new ColBox(360, 20, 0, 160,"#606060");
+var blimsyArray = [blimsy];
 
 function loop(){
-    console.log(vadid.collisionTest(blimsy));
+// Movement controls for Vadid and Blimsy
     controller.controls(vadid);
     worldFunctions.moveBlimsy(blimsy, vadid);
-    //background
+//Rendering Background
     context.fillStyle = "#202020";
     context.fillRect(0,0,320,180);
 
+    ground.draw();
+    block.draw();
+    block2.draw()
+    vadid.draw();
+    blimsyArray[0].draw();
+    rectangle.draw();
+
+// Blimsy detecion 
     if (vadid.collisionTest(blimsy)){
         blimsy.color= "#ffff00aa";
     } else {
         blimsy.color= "#00ff00";
     }
 
-
-    block.draw();
-    vadid.draw();
-    blimsy.draw();
+// world collision detection    
     if (block.collisionDetection(vadid)){
         block.collisionRessolve(vadid);
     }
-    
+    if (block.collisionDetection(blimsy)){
+        block.collisionRessolve(blimsy);
+    }
+    if (rectangle.collisionDetection(vadid)){
+        rectangle.collisionRessolve(vadid);
+    }
+    if (block2.collisionDetection(vadid)){
+        block2.collisionRessolve(vadid);
+    }
+    if (rectangle.collisionDetection(blimsy)){
+        rectangle.collisionRessolve(blimsy);
+    }
+    if (ground.collisionDetection(vadid)){
+        ground.collisionRessolve(vadid);
+    }
+    if (ground.collisionDetection(blimsy)){
+        ground.collisionRessolve(blimsy);
+    }
+
     worldFunctions.checkBounds(vadid);
     worldFunctions.checkBounds(blimsy);
     window.requestAnimationFrame(loop);
